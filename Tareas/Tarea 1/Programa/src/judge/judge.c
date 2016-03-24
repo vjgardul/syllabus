c9866e7fb556c6dab7bdc9bbfb2b37818a662afe
Vicente Errázuriz <verrazuriz@uc.cl>
2016-03-24 11:43:13 -0300

    Codigo base T1

---
+++ Tareas/Tarea 1/Programa/src/judge/judge.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main(int argc, char** argv)
+{
+    fprintf(stderr, "%s\n", "Estamos trabajando para usted");
+    fprintf(stderr, "%s\n", "(Pronto se subirá el juez)");
+
+    return 1;
+}
---
+++ Tareas/Tarea 1/Programa/src/solver/solver.c
@@ -0,0 +1,20 @@
+#include "../common/city.h"
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* Leemos la ciudad del input */
+    Layout* layout = city_layout_read(stdin);
+
+    /* Lo imprimimos para darle el estado inicial al watcher / judge */
+    city_layout_print(layout);
+
+    /* TODO RESOLVER PROBLEMA */
+
+    /* TODO IMPRIMIR DECISIONES */
+
+    /* Liberamos memoria */
+    city_layout_destroy(layout);
+
+    return 0;
+}
